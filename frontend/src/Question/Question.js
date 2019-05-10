7bd7ee3b13870ec6405ce43607e5b8edad98801f
danitiemi <danitiemi.br@gmail.com>
2019-05-10 07:50:38 -0700

    Feature: Display Questions

--- frontend/src/Question/Question.js
+++ frontend/src/Question/Question.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import SubmitAnswer from './SubmitAnswer'
+import auth0Client from '../Auth'
 
 class Question extends Component {
   constructor(props) {
@@ -7,9 +9,15 @@ class Question extends Component {
     this.state = {
       question: null,
     };
+
+    this.submitAnswer = this.submitAnswer.bind(this);
   }
 
   async componentDidMount() {
+    await this.refreshQuestion();
+  }
+
+  async refreshQuestion() {
     const { match: { params } } = this.props;
     const question = (await axios.get(`http://localhost:8081/${params.questionId}`)).data;
     this.setState({
@@ -17,11 +25,20 @@ class Question extends Component {
     });
   }
 
+  async submitAnswer(answer) {
+    await axios.post(`http://localhost:8081/answer/${this.state.question.id}`, {
+      answer,
+    }, {
+      headers: { 'Authorization': `Bearer ${auth0Client.getIdToken()}` }
+    });
+    await this.refreshQuestion();
+  }
+
   render() {
     const {question} = this.state;
     if (question === null) 
       return <p>Loading ...</p>;
-      
+
     return (
       <div className="container">
         <div className="row">
@@ -29,6 +46,7 @@ class Question extends Component {
             <h1 className="display-3">{question.title}</h1>
             <p className="lead">{question.description}</p>
             <hr className="my-4" />
+            <SubmitAnswer questionId={question.id} submitAnswer={this.submitAnswer} />
             <p>Answers:</p>
             {
               question.answers.map((answer, idx) => (
